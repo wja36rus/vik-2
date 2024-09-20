153c8c92bfa1b3c30ba06ad95dc713182b2f27ef
wja36rus <48429607+wja36rus@users.noreply.github.com>
2024-09-20 10:51:48 +0300

    forms

---
+++ script/mail.php
@@ -0,0 +1,24 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$text = $_POST["text"];
+
+$to  = "<mail@example.com>, "; 
+
+$subject = "Новое письмо с сайта НПК VIK"; 
+
+$message = "
+<b>Имя из формы </b><span>$name</span> </br>
+<b>E-mail из формы </b><span>$email</span> </br>
+<b>Текст из формы </b><span>$text</span>";
+
+
+$headers  = "Content-type: text/html; charset=windows-1251 \r\n"; 
+$headers .= "From: НПК VIK\r\n"; 
+
+if(mail($to, $subject, $message, $headers)) {
+    echo json_encode(["status" => "success"]);
+} else {
+    echo json_encode(["status" => "error"]);
+}
--- script/script.js
+++ script/script.js
@@ -5,12 +5,46 @@ $(document).ready(function () {
     const email = $("#email").val();
     const text = $("#text").val();
 
-    if (text === "") {
-      $("#name-error").append("Введите имя");
+    if (name === "") {
+      $("#error-name").text("Введите имя");
+      return;
+    } else {
+      $("#error-name").text("");
     }
 
-    console.log("### ~ text:", text);
-    console.log("### ~ name:", name);
-    console.log("### ~ email:", email);
+    if (email === "") {
+      $("#error-email").text("Введите e-mail");
+      return;
+    } else {
+      $("#error-email").text("");
+    }
+
+    const body = {
+      text: text,
+      name: name,
+      email: email,
+    };
+
+    $.ajax({
+      type: "post",
+      url: "../script/mail.php",
+      data: body,
+      dataType: "json",
+      success: function ({ status }) {
+        if (status === "success") {
+          console.log("Success!");
+          $("#name").val("");
+          $("#email").val("");
+          $(".success-form").addClass("active");
+
+          setTimeout(() => {
+            $(".success-form").removeClass("active");
+          }, 2500);
+        }
+      },
+      error: function (error) {
+        console.log("### ~ error:", error);
+      },
+    });
   });
 });
